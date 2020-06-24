2cc879fdffafae0b4f7383cb83f2af77a0965980
Yu Ishihara <ishihara.yu@gmail.com>
2020-06-24 23:34:38 +0900

    Update

--- app/javascripts/index.js
+++ app/javascripts/index.js
@@ -2,20 +2,3 @@ import '../stylesheets/styles.scss';
 
 import $ from 'jquery';
 import 'bootstrap';
-import Vue from "vue"
-import Hello from './components/Hello.vue'
-
-$(function() {
-  console.log("ready");
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-  var app = new Vue({
-    el: "#app",
-    render: h => h(Hello, {
-      attrs: {
-        message: 'hello!'
-      }
-    })
-  });
-});
--- webpack/base.config.js
+++ webpack/base.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
 module.exports = {
   context: path.resolve(__dirname, '../app'),
@@ -31,10 +30,6 @@ module.exports = {
         ]
       },
       {
-        test: /\.vue$/,
-        loader: "vue-loader",
-      },
-      {
         test: /\.js$/,
         exclude: /node_modules/,
         use: 'babel-loader'
@@ -43,21 +38,13 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin(),
-    new VueLoaderPlugin(),
     new HtmlWebpackPlugin({
       template: './html/index.ejs',
       minify: {
         removeComments: true,
         collapseWhitespace: true,
         removeAttributeQuotes: true
-      },
-      chunksSortMode: 'dependency'
+      }
     })
-  ],
-  resolve: {
-    extensions: [".vue", ".js"],
-    alias: {
-      "vue$": "vue/dist/vue.esm.js"
-    }
-  }
+  ]
 }
